a95391f8c0a44f08bf37fc3be31b559fa9f1d2ed
Pierre Le Bever <pierrelebever@gmail.com>
2019-08-20 15:56:54 +0200

    Audio added to rocket and Level 1

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rocketRigidbody;
+    AudioSource rocketAudio;
 
     // Start is called before the first frame update
     void Start()
     {
         rocketRigidbody = GetComponent<Rigidbody>();
+        rocketAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,7 +26,16 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rocketRigidbody.AddRelativeForce(Vector3.up);
+            if (!rocketAudio.isPlaying)
+            {
+                rocketAudio.Play();
+            }
         }
+        else
+        {
+            rocketAudio.Stop();
+        }
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Rotate(Vector3.forward * Time.deltaTime * 100);
